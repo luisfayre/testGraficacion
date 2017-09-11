8c54cd9fbec47a8fa3bc33aaa8a8813046fa7e40
luisfayre <15551452@lc_047.itchii.lab>
2017-09-11 09:51:20 -0600

    circulo
    Circulo

--- bonsaijs/www/js/app.js
+++ bonsaijs/www/js/app.js
@@ -1,4 +1,4 @@
-new Rect(10, 10, 100, 100)
+/*new Rect(10, 10, 100, 100)
 .addTo(stage)
 .attr('fillColor', 'red')
 .animate('5s', {
@@ -7,6 +7,14 @@ new Rect(10, 10, 100, 100)
     delay: '5s'  
   }
 )
-  .animate('5s', {
+.animate('5s', {
     x: -700,
-  });
+  }); */
+
+new Circle(500, 500, 100)
+	.addTo(stage)
+	.attr('fillColor', 'red');
+
+new Arc(300, 300, 100, 0, 50, 300)
+	.addTo(stage)
+	.attr('fillColor', 'red');
